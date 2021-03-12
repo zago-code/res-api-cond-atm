ea9bed0fa9409172b60f426727220624c5906081
Gabriel Zarate <gzaratet1@gmail.com>
2021-03-11 19:33:41 -0600

    [ADD] pagination by block

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,10 +2,18 @@
 import React from 'react';
 
 function Pagination(props) {
-  const { prev, next } = props;
+  const { prev, next, page, maxPage, block, onNext, onPrev } = props;
   return (
     <nav>
       <ul>
+        <li>
+          <span>{`DataBlock: ${block}`}</span>
+        </li>
+        <li>
+          <a href='#' onClick={onPrev}>
+            <span>prev-block</span>
+          </a>
+        </li>
         <li>
           <a href='#' onClick={prev}>
             <span aria-hidden='true'>&laquo;</span>
@@ -13,9 +21,17 @@ function Pagination(props) {
           </a>
         </li>
         <li>
+          <span>{`${page} of ${maxPage}`}</span>
+        </li>
+        <li>
           <a href='#' onClick={next}>
             <span aria-hidden='true'>&raquo;</span>
             {/* <span className='sr-only'>Next</span> */}
+          </a>
+        </li>
+        <li>
+          <a href='#' onClick={onNext}>
+            <span>next-block</span>
           </a>
         </li>
       </ul>
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-// import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import TableRows from './TableRows';
 import TableHeader from './TableHeader';
 import Pagination from './Pagination';
@@ -6,9 +6,23 @@ import useGetData from '../hooks/useGetData';
 import usePagination from '../hooks/usePagination';
 
 function Table() {
-  const dataObj = useGetData(1);
+  const [blockData, setBlockData] = useState(1);
+  const dataObj = useGetData(blockData);
   let data = [];
-  if (dataObj.results) data = dataObj.results;
+  let maxBlock = 0;
+  if (dataObj.results) {
+    maxBlock = Math.round(dataObj.pagination.total / 100);
+    data = dataObj.results;
+  }
+
+  function handleChangeBlockNext(even) {
+    setBlockData((blockData) => Math.min(blockData + 1, maxBlock));
+  }
+
+  function handleChangeBlockPrev(even) {
+    setBlockData((blockData) => Math.max(blockData - 1, 1));
+  }
+
   const dataFilter = usePagination(data, 10);
   if (dataFilter.currentData()) data = dataFilter.currentData();
 
@@ -26,7 +40,15 @@ function Table() {
         <TableHeader />
         <TableRows data={data} />
       </table>
-      <Pagination prev={handleChangePrevious} next={handleChangeNext} />
+      <Pagination
+        page={dataFilter.currentPage}
+        maxPage={dataFilter.maxPage}
+        prev={handleChangePrevious}
+        next={handleChangeNext}
+        block={blockData}
+        onNext={handleChangeBlockNext}
+        onPrev={handleChangeBlockPrev}
+      />
     </>
   );
 }
